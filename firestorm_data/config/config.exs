5da8b4d2cea8baf64921ddd75421c6d7f882bd94
vishalgautamm <vishal.g@live.ca>
2017-05-27 10:58:43 -0400

    Refactored config.exs

--- firestorm_data/config/config.exs
+++ firestorm_data/config/config.exs
@@ -2,13 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :firestorm_data, FirestormData.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  database: "firestorm_data_repo",
-  username: "user",
-  password: "pass",
-  hostname: "localhost"
-
 config :firestorm_data,
   ecto_repos: [FirestormData.Repo]
 
