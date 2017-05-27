254d552c06e45e3ef536c46e61a928f69eb0d17f
vishalgautamm <vishal.g@live.ca>
2017-05-26 21:05:17 -0400

    Passed third test

--- otp_servers/test/fridge_server_test.exs
+++ otp_servers/test/fridge_server_test.exs
@@ -10,7 +10,13 @@ defmodule FridgeServer do
   end
 
   def handle_call({:take, item}, _from, items) do
-    {:reply, {:ok, item}, items}
+    case Enum.member?(items, item) do
+      true ->
+        {:reply, {:ok, item}, List.delete(items, item)}
+      false ->
+        {:reply, :not_found, items}
+    end
+    
   end
 
 end
