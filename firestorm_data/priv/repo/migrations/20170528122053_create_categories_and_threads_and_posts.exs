0c47b96928af4ad538fcac70f7513b99a0f04344
vishalgautamm <vishal.g@live.ca>
2017-05-28 08:26:38 -0400

    Table for Categories, with title

--- firestorm_data/priv/repo/migrations/20170528122053_create_categories_and_threads_and_posts.exs
+++ firestorm_data/priv/repo/migrations/20170528122053_create_categories_and_threads_and_posts.exs
@@ -2,6 +2,10 @@ defmodule FirestormData.Repo.Migrations.CreateCategoriesAndThreadsAndPosts do
   use Ecto.Migration
 
   def change do
+    create table(:categories) do
+      add :title, :string
 
+      timestamps()
+    end
   end
 end
