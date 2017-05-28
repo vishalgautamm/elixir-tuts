4b17ce005970198d426853d6231b44a043237a8b
vishalgautamm <vishal.g@live.ca>
2017-05-28 09:24:01 -0400

    Added post Schema

---
+++ firestorm_data/lib/firestorm_data/schema/post.ex
@@ -0,0 +1,18 @@
+defmodule FirestormData.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "posts" do
+    field :body, :string
+    # belongs_to specift that this thread has a parent category
+    belongs_to :thread, FirestormData.Thread
+    belongs_to :user, FirestormData.User
+
+    timestamps()
+  end
+
+  def changeset(post, params \\ %{}) do
+    post
+    |> cast(params, [:body, :thread_id, :user_id])
+  end
+end
