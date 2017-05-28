178e04ade5aedb9e62afc78b940c47644bc118b9
vishalgautamm <vishal.g@live.ca>
2017-05-28 08:35:32 -0400

    Posts table, which belong to a thread and a user, and has a body(text)

--- firestorm_data/priv/repo/migrations/20170528122053_create_categories_and_threads_and_posts.exs
+++ firestorm_data/priv/repo/migrations/20170528122053_create_categories_and_threads_and_posts.exs
@@ -9,13 +9,23 @@ defmodule FirestormData.Repo.Migrations.CreateCategoriesAndThreadsAndPosts do
     end
 
     create table(:threads) do
-      add :categpry_id, referemces(:categories)
+      add :categpry_id, references(:categories)
       add :title, :string
 
       timestamps()
     end
 
     create index(:threads, [:categpry_id])
+    # Posts belong to a thread and a user, and have a body
+    create table(:posts) do
+      add :thread_id, references(:threads)
+      add :body, :text
+      add :user_id, references(:users)
 
+      timestamps()
+    end
+    # Index posts by thread and user
+    create index(:posts, [:thread_id])
+    create index(:posts, [:user_id])
   end 
 end
