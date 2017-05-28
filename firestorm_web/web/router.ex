9bcfd4a696359e9ef7b7d46175b11bc5789f736e
vishalgautamm <vishal.g@live.ca>
2017-05-28 16:14:45 -0400

    Added documentation

--- firestorm_web/web/router.ex
+++ firestorm_web/web/router.ex
@@ -1,6 +1,9 @@
 defmodule FirestormWeb.Router do
   use FirestormWeb.Web, :router
 
+  # In Phoenix you set up pipelines for various plugs.
+  # Plugs take a `Plug.Conn` and modify it - this is the default browser stack
+  # that Phoenix installs.
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -9,13 +12,19 @@ defmodule FirestormWeb.Router do
     plug :put_secure_browser_headers
   end
 
+  # Here's a separate default pipeline for API endpoints
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  # When a request comes through for the root, this scope is applied.
+  # The second argument it the default namespace that we will use internally -
+  # this way we can avoid prefixing everything with FirestormWeb.Web
   scope "/", FirestormWeb do
     pipe_through :browser # Use the default browser stack
 
+    # When someone `GET`s the root path, we'll handle the request with the
+    # `PageController`'s `index` function.
     get "/", PageController, :index
   end
 
